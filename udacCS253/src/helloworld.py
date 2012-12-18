a47a3a776838fd2f9c07377558aa6124b9a7b7f6
Ash Booth <ash.booth@soton.ac.uk>
2012-12-18 21:23:31 +0000

    Started front form for ascichan

--- udacCS253/src/helloworld.py
+++ udacCS253/src/helloworld.py
@@ -5,7 +5,8 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 import os
 import re
-# from string import letters
+
+from google.appengine.ext import db
 
 import webapp2
 import jinja2
@@ -14,14 +15,14 @@ import cgi
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
-        
-def render_str(template, **params):
-    t = jinja_env.get_template(template)
-    return t.render(params)
 
 class BaseHandler(webapp2.RequestHandler):
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+    
     def render(self, template, **kw):
-        self.response.out.write(render_str(template, **kw))
+        self.response.out.write(self.render_str(template, **kw))
 
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
