87c35917b4118d7ded77f6d6b302da4659d7321f
Ash Booth <ash.booth@soton.ac.uk>
2012-12-21 10:13:17 +0000

    Added cookie and password encrypting

--- udacCS253/src/helloworld.py
+++ udacCS253/src/helloworld.py
@@ -6,11 +6,17 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 import os
 import re
 
+SECRET = "EDCBA"
+
 from google.appengine.ext import db
 
 import webapp2
 import jinja2
 import cgi
+import hashlib
+import hmac
+import random
+import string
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
@@ -19,6 +25,37 @@ jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
 def render_str(template, **params):
         t = jinja_env.get_template(template)
         return t.render(params)
+
+
+###### Cookie Encrypting #######
+def hash_str(s):
+    return hmac.new(SECRET, s).hexdigest()
+    return hashlib.md5(s).hexdigest()
+
+def make_secure_val(s):
+    return "%s|%s" % (s, hash_str(s))
+
+def check_secure_val(h):
+    val = h.split('|')[0]
+    if h == make_secure_val(val):
+        return val
+    
+###### Password Encrypting #######
+def make_salt():
+    return ''.join(random.choice(string.letters) for x in xrange(5))
+
+def make_pw_hash(name, pw, salt=None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s,%s' % (h, salt)
+
+def valid_pw(name, pw, h):
+    salt = h.split(',')[1]
+    return h == make_pw_hash(name, pw, salt)
+    
+
+##################################
 
 class BaseHandler(webapp2.RequestHandler):
     def render_str(self, template, **params):
@@ -31,16 +68,27 @@ class BaseHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
         
-class MainPage(webapp2.RequestHandler): 
-    def write_form(self):
-        self.response.out.write("Main Page")
-
+class MainPage(BaseHandler): 
     def get(self):
-        self.write_form()
+        self.response.headers['Content-Type'] = 'text/plain'
+        visits = 0
+        visit_cookie_str = self.request.cookies.get('visits', 0)
+        if visit_cookie_str:
+            cookie_val = check_secure_val(visit_cookie_str)
+            if cookie_val:
+                visits = int(cookie_val)
+        visits += 1
+        new_cookie_val = make_secure_val(str(visits))
+        self.response.headers.add_header('Set-Cookie', "visits=%s" % new_cookie_val)
+        if visits > 150:
+            self.write("Awesome!")
+        else:
+            self.write("You have been here {} times!".format(visits))
       
     def post(self):
         the_string = self.request.get('text')
         self.write_form(the_string)
+
         
 class Artwork(db.Model):
     title = db.StringProperty(required=True)
